a0e68a39891970e239046207e5e03141f9c3e043
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-01 01:32:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def line(array)
+  phrase="The line is currently"
+  if array.size==0
+    phrase<<" empty."
+  elsif array.size>=1
+    phrase<<":"
+    array.each_with_index do|person, index|
+    phrase<<" #{index+1}. #{person}"
+  end
+end
+puts phrase
+end
+
+def take_a_number(katz_deli, nameofnewperson)
+  katz_deli<<"#{nameofnewperson}"
+  phrase="Welcome, #{nameofnewperson}. You are number #{katz_deli.size} in line."
+  puts phrase
+end
+
+def now_serving(katz_deli)
+  if katz_deli.size==0 
+    puts "There is nobody waiting to be served!"
+  else
+    katz_deli.each do |person|
+    puts "Currently serving #{person}."
+    katz_deli.shift
+    end
+  end
+end
